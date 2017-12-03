1d8849406de070a5899dcd9ecc11da68b4094802
Vlad Vytrykush <foxifio00@gmail.com>
2017-12-03 23:10:24 +0200

    add test for club endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import eventRoutes from './event.routes';
 import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
+import clubRoutes from './club.routes';
 
 export default {
   authRoutes,
   eventRoutes,
   userRoutes,
+  clubRoutes,
 };
---
+++ test/data/club.data.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  name: 'fakeName',
+  address: 'fakeAddress',
+  description: 'fakeDescription',
+  email: 'fakeEmail@gmail.com',
+  phoneNumber: 123456789,
+  photosUrl: ['http://fakeimg.pl/300/','http://fakeimg.pl/300/'],
+  siteUrl: 'http://fakesite.com/',
+};
